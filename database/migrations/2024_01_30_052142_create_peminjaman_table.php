5ee3e59e606356084d534b13a77e9f547f159591
intannuraeni83 <intannuraeni83>
2024-02-13 10:46:12 +0700

    myelesaikan login_logout_registrasi

---
+++ database/migrations/2024_01_30_052142_create_peminjaman_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('buku_id')->constrained('buku');
+            $table->date('TaggalPeminjaman');
+            $table->date('TaggalPengembalian');
+            $table->string('StatusPeminjaman');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
--- resources/views/template_back/index.blade.php
+++ resources/views/template_back/index.blade.php
@@ -214,7 +214,7 @@
 		<!-- Footer opened -->
 		<div class="main-footer ht-45">
 			<div class="container-fluid pd-t-0-f ht-100p">
-					<span> Copyright © 2022 <a   href="javascript:void(0);" class="text-primary">Azira</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a   href="javascript:void(0);"> Spruko </a> All rights reserved.</span>
+					<span> Copyright © 2022 <a   href="javascript:void(0);" class="text-primary">INTAN</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a   href="javascript:void(0);"> INTANNUR'AENI</a> All rights reserved.</span>
 			</div>
 		</div>
 		<!-- Footer closed -->
